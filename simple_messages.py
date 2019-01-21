acc1f05f4e766088f5c4cf301f7c4595c2325dfb
Mariano Esturrica Mayo <mariano_esturrica@hotmail.com>
2019-01-21 20:28:30 +0100

    Add files via upload

---
+++ simple_messages.py
@@ -0,0 +1,34 @@
+message = "Hello test"
+print(message.lower())
+print(message.upper())
+print(message.title())
+
+message = "Bye"
+print(message)
+
+
+#Personal Message 
+name = "Eric"
+
+print("Personal message " + name + "?")
+
+print("prova de l'apostrof")
+
+print('he said "Hello"')
+
+print(5+3)
+print(11-3)
+print(24/3)
+print(4*2)
+
+
+message = 24
+
+print("El meu numero preferit es " + str(message));
+
+bicycles = ['bici1','bici2','bici3']
+print(bicycles[0])
+
+
+#retorna el ultim element de la llista
+print(bicycles[-1])
